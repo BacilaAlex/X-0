636c62da669472916c1a5b21c33fe0f6bff712b1
Alex Bacila <bacila.a2002@yahoo.com>
2023-03-23 21:56:22 +0200

    Am facut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatratComponent } from './patrat/patrat.component';
-import { TablaComponent } from './tabla/tabla.component';
 import { TableScoreComponent } from './table-score/table-score.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PatratComponent,
-    TablaComponent,
-    TableScoreComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PatratComponent, TableScoreComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
